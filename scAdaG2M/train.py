0788704fdb8fcbb35f9bf59334bf97aedd1d8a74
momo <108646959+gxl1110@users.noreply.github.com>
2026-08-07 02:48:44 +0800

    Delete scAdaG2M directory

--- scAdaG2M/train.py
+++
@@ -1,9 +0,0 @@
-from config import parse_config
-from train_scrna_unsup import run
-import torch
-
-if __name__ == "__main__":
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-        torch.cuda.reset_peak_memory_stats()
-    run(parse_config())
